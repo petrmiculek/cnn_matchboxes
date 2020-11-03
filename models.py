b71b982c0e6226ed2e3234dcd270f25d352d2155
petrmiculek <petr.miculek@gmail.com>
2020-11-03 21:20:20 +0100

    load&save model, plot confusion matrix, save misclassified regions

--- models.py
+++ models.py
@@ -6,15 +6,19 @@ def conv_tutorial(num_classes):
         tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255),
         tf.keras.layers.Conv2D(32, 3, activation='relu'),
         tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(32, 3, activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(32, 3, activation='relu'),
 
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
         tf.keras.layers.MaxPooling2D(),
+
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+
         tf.keras.layers.Flatten(),
+        # tf.keras.layers.Dropout(0.2),
+
         tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(num_classes)
-    ])
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ], name='sequential_3conv_32channels')
 
 
 def conv_tutorial_tweaked(num_classes):
@@ -22,11 +26,8 @@ def conv_tutorial_tweaked(num_classes):
         [
             tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255),
 
-            tf.keras.layers.Conv2D(4, 3, activation='relu'),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-
             tf.keras.layers.Conv2D(8, 3, activation='relu'),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),  # == default pool_size
 
             tf.keras.layers.Conv2D(16, 3, activation='relu'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
@@ -34,11 +35,15 @@ def conv_tutorial_tweaked(num_classes):
             tf.keras.layers.Conv2D(32, 3, activation='relu'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
+            tf.keras.layers.Conv2D(64, 3, activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(128, activation='relu'),
             # tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(num_classes),
-            tf.keras.layers.Softmax()
+            # tf.keras.layers.Softmax()
 
         ]
+        , name='sequential_4conv_increasing_channels'
     )
